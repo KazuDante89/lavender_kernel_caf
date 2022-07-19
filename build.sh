daa5cbd19f3b4987346267b36d7660a82b5c4059
KazuDante89 <karlemmanueldorvil@gmail.com>
2022-07-19 11:12:52 -0400

    Testing ... 	modified:   arch/arm64/configs/lavender-perf_defconfig 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -48,7 +48,7 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="Test_v00"
+ZIPNAME="Test_v01"
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
