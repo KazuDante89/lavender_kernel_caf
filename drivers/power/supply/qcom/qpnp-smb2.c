188f0d6bc062d5e7a4673e040942f4ac3d238ee9
KazuDante89 <karlemmanueldorvil@gmail.com>
2022-07-19 00:37:40 -0400

    changes 	new file:   .ci.yml 	modified:   .gitignore 	modified:   arch/arm64/configs/lavender-perf_defconfig 	new file:   build.sh 	modified:   drivers/power/supply/qcom/qpnp-fg-gen3.c 	modified:   drivers/power/supply/qcom/qpnp-smb2.c 	modified:   drivers/power/supply/qcom/smb-lib.c 	new file:   regen.sh

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -262,6 +262,10 @@ static int smb2_parse_dt(struct smb2 *chip)
 				"qcom,fcc-max-ua", &chg->batt_profile_fcc_ua);
 	if (rc < 0)
 		chg->batt_profile_fcc_ua = -EINVAL;
+#ifdef CONFIG_MACH_LONGCHEER
+	if (hwc_check_global) {
+		chg->batt_profile_fcc_ua = 2900000;
+#endif		
 	}
 	rc = of_property_read_u32(node,
 				"qcom,fv-max-uv", &chg->batt_profile_fv_uv);
